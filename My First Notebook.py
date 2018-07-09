99860f9defd3a8cf9d86bf5add21f44e3091f4f5
Hüseyin Emecan <emecanh@gmail.com>
2018-07-09 18:54:31 +0300

    first Udemy Jupyter Notebook

---
+++ My First Notebook.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+for x in range(1,5):
+    print( x , ' Hello World! ')
+    
+
+
+# In[4]:
+
+
+var = 12
+num = 3
+say = 'This equetions is'
+
+total = var**num
+
+print(say, total)
+
+
+# In[5]:
+
+
+23+34
+
+
+# In[6]:
+
+
+print(23+34)
+
